bb3ab22def4b50b05ed3b4276cc969eab879418d
mrursu <mr0ursu>
2023-09-28 00:44:56 +0200

    test4

--- inno_parser/main.py
+++ inno_parser/main.py
@@ -5,7 +5,7 @@ import math
 
 # text 3 
 # ursu
-
+# test4
 headers = {
     "Accept": "*/*",
     "User-Agent": "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1"
